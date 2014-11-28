d99df72d76ed0a82f9642dba87f61c248d6076ec
Jack Leigh <t.j.j.l.1997@gmail.com>
2014-11-27 23:13:57 -0400

    Error checking in scanner input
    Added try-catch statement to GettingAnswersByScanner to prevent
    incorrect inputs, and also removed an extra input statement that was
    unnecessary.

--- src/com/rns/graduation/GettingAnswersByScanner.java
+++ src/com/rns/graduation/GettingAnswersByScanner.java
@@ -1,5 +1,6 @@
 package com.rns.graduation;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GettingAnswersByScanner {
@@ -11,22 +12,29 @@ public class GettingAnswersByScanner {
 
 		String firstName;
 		String lastName;
-		int age;
-		
+		int age = 0;
+		boolean cont = false;
 		int currentYear = 2014;
 		int graduationYear = 0;
 		int ageAtGraduation = 18;
 
-		System.out.println( "what's your first name? " );
+		System.out.println( "What's your first name? " );
 		firstName = keyboard.next();
 
-		System.out.println( "what's your last name? " );
+		System.out.println( "What's your last name? " );
 		lastName = keyboard.next();
 
-		System.out.println( "how old are you? " );
-		keyboard.nextInt();
-
+		System.out.println( "How old are you? " );
+		//keyboard.nextInt();
+		do{
+		
+		try{
 		age = Integer.parseInt(keyboard.next());
+		cont = true;
+		}catch(InputMismatchException|NumberFormatException e){
+			System.out.println("Please input an integer!");
+		}
+		}while(!cont);
 		
 		int yearsToGraduate = ageAtGraduation - age;
 		graduationYear = currentYear + yearsToGraduate;
