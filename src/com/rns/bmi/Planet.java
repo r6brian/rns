798ce1bfe91e14ec0b1e089661b426393cbd4e76
Jack Leigh <t.j.j.l.1997@gmail.com>
2014-11-29 20:57:16 -0400

    Enum to represent planet's gravity

---
+++ src/com/rns/bmi/Planet.java
@@ -0,0 +1,19 @@
+package com.rns.bmi;
+
+public enum Planet{
+	EARTH(1.0),
+	VENUS(0.78),
+	MARS(0.39),
+	JUPITER(2.65),
+	SATURN(1.17),
+	URANUS(1.05),
+	NEPTUNE(1.23);
+
+private double GForce;
+
+double getGForce(){return GForce;}
+
+private Planet(double G){
+	this.GForce = G;
+};
+}
